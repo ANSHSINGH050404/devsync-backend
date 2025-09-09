8b274eb789e94b7c74cd1fe8530a22d4475605a7
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2025-09-10 04:04:01 +0530

    add origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://devsync-backend-m35p.onrender.com/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true // needed if using cookies/sessions
 }));
